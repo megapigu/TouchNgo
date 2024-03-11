137a4f2bdea5b0c2ce85a4324231854884c5d0c2
megapigu <121689047+megapigu@users.noreply.github.com>
2024-03-11 17:31:25 +0800

    Add files via upload

---
+++ html/results.js
@@ -0,0 +1,32 @@
+// const paragraph = document.querySelector('#paragraph');
+
+// const params = new URLSearchParams(window.location.search);
+
+// params.forEach((value, key)=>{
+//     paragraph.append(`${key} = ${value}`);
+//     paragraph.append(document.createElement('br'));
+// });
+
+
+const usernameDisplay = document.querySelector('#usernameDisplay');
+const myKadNoDisplay = document.querySelector('#myKadNoDisplay');
+
+const params = new URLSearchParams(window.location.search);
+
+params.forEach((value, key) => {
+    if (key === 'username') {
+        usernameDisplay.textContent = `${value}`;
+    } else if (key === 'myKadNo') {
+        myKadNoDisplay.textContent = `${value}`;
+    }   
+});
+
+function getSelectValue()
+{
+    var selectedValue = document.getElementById("State").value;
+    console.log(selectedValue);
+    var selectedValue = document.getElementById("gender").value;
+    console.log(selectedValue);
+    var selectedValue = document.getElementById("race").value;
+    console.log(selectedValue);
+}
