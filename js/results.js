de6c04f6122ee17266c849897c2ca37fb617e3d8
megapigu <121689047+megapigu@users.noreply.github.com>
2024-03-16 16:17:06 +0800

    Update results.js

--- js/results.js
+++ js/results.js
@@ -1,13 +1,3 @@
-// const paragraph = document.querySelector('#paragraph');
-
-// const params = new URLSearchParams(window.location.search);
-
-// params.forEach((value, key)=>{
-//     paragraph.append(`${key} = ${value}`);
-//     paragraph.append(document.createElement('br'));
-// });
-
-
 const usernameDisplay = document.querySelector('#usernameDisplay');
 const myKadNoDisplay = document.querySelector('#myKadNoDisplay');
 
